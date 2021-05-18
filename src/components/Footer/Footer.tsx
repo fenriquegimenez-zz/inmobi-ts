47c0242db81319036aa474802185b0653de45cf9
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-05-18 19:40:00 -0400

    buttons added to home page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-light text-center text-secondary py-3">
+    <footer className="bg-light text-center text-secondary my-5 py-3">
       <small>Inmobi 2.0</small>
     </footer>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,35 @@
 import React from "react"
+import Link from "next/dist/client/link"
 
 const home = () => {
-  return <h1 className="text-center">Home</h1>
+  return (
+    <>
+      <h2 className="text-center my-4">Home</h2>
+      <br />
+      <p>
+        Inmobi es una moderno sistema de gestión de clientes y cobranzas
+        construido bajo los últimos estándares de calidad en la industria del
+        desarrollo de software.{" "}
+        <span>
+          (Para más detalles contacte al{" "}
+          <a
+            className="text-secondary text-decoration-none"
+            href="mailto:fenriquegimenez@gmail.com"
+          >
+            desarrollador
+          </a>
+          )
+        </span>
+      </p>
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <Link href="cobranzas">
+          <button className="btn btn-secondary">Ir a Cobranzas</button>
+        </Link>
+        <Link href="customers">
+          <button className="btn btn-secondary">Ir a Clientes</button>
+        </Link>
+      </div>
+    </>
+  )
 }
 export default home
